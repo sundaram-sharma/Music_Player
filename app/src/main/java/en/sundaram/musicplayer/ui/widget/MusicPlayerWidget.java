34b84b38d14507a1bb5947e230df03f82e853946
Sundaram Sharma <sundaram-sharma@hotmail.com>
2022-11-21 03:09:09 +0530

    Widget Created

---
+++ app/src/main/java/en/sundaram/musicplayer/ui/widget/MusicPlayerWidget.java
@@ -0,0 +1,31 @@
+package en.sundaram.musicplayer.ui.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class MusicPlayerWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+
+
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+}
